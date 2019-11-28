4704f6f603d86e028fa6163880a4270bcdbd5912
Viktorija Valsoe <viktorijavalsoe@gmail.com>
2019-11-28 16:54:00 +0100

    added firebase

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
-
+import firebase from '../services/firebase';
 import SEO from '../components/seo';
 
-const IndexPage = () => (
-  <div>
-    <SEO title="Home" />
-    <h1>This is sourceful</h1>
-  </div>
-);
+const IndexPage = () => {
+  let db = firebase.firestore();
+
+  console.log(db);
+  console.log(db.collection('factories'));
+  db.collection('factories')
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data().name}`);
+      });
+    });
+  return (
+    <div>
+      <SEO title="Home" />
+
+      <h1>This is sourceful</h1>
+    </div>
+  );
+};
 
 export default IndexPage;
