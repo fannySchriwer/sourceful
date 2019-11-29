cc968c677514458d8323b8e6ee1af57f76db67ab
Viktorija Valsoe <viktorijavalsoe@gmail.com>
2019-11-29 15:59:02 +0100

    deleed node modules ,and reinstalled everything

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,14 @@
-import React from "react"
-import SEO from "../components/Seo"
-import FactoryList from "../components/FactoryList"
+import React from 'react';
+import SEO from '../components/Seo';
+import FactoryList from '../components/FactoryList';
 
-const IndexPage = () => {
-  return (
-    <div>
-      <SEO title="Home" />
+const IndexPage = () => (
+  <div>
+    <SEO title="Home" />
 
-      <h1>This is sourceful</h1>
-      <FactoryList />
-    </div>
-  )
-}
+    <h1>This is sourceful</h1>
+    <FactoryList />
+  </div>
+);
 
-export default IndexPage
+export default IndexPage;
--- src/utils/useFactories.js
+++ src/utils/useFactories.js
@@ -1,32 +1,28 @@
-import { useState, useEffect } from "react"
-import firebase from "../services/firebase"
+import { useState, useEffect } from 'react';
+import firebase from '../services/firebase';
 
 export default function useFactories() {
-  const [factories, setFactories] = useState([])
-  const [error, setError] = useState(false)
+  const [factories, setFactories] = useState([]);
 
   useEffect(() => {
-    const db = firebase.firestore()
+    const db = firebase.firestore();
     const unsubscribe = db
-      .collection("factories")
-      //add some sorting logic
+      .collection('factories')
+      // add some sorting logic
       // .where("continent", "==", "asia")
       .get()
-      .then(function(querySnapshot) {
-        const newFactories = querySnapshot.docs.map(doc => ({
+      .then((querySnapshot) => {
+        const newFactories = querySnapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
-        }))
-        err => {
-          setError(err)
-        }
-        setFactories(newFactories)
-      })
+        }));
+        setFactories(newFactories);
+      });
 
     // returning the unsubscribe function will ensure that
     // we unsubscribe from document changes when we leave component
-    return () => nsubscribe()
-  }, [])
+    return () => unsubscribe();
+  }, []);
 
-  return { factories, error }
+  return { factories };
 }
