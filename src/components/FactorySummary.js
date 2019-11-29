14c5d983907e8590b588bfae9a1f7fc158162eb8
Viktorija Valsoe <viktorijavalsoe@gmail.com>
2019-11-29 14:16:39 +0100

    added get factories from firestore function, and factoryComponents

---
+++ src/components/FactorySummary.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const FactorySummary = ({ factory }) => {
+  console.log(factory)
+  return (
+    <div key={factory.id}>
+      <div>
+        <h1>{factory.name}</h1>
+        <p>{factory.country}</p>
+      </div>
+      <div>
+        <p>Number of employees</p>
+        {factory.employee}
+      </div>
+
+      <p>{factory.summary}</p>
+      <div>Read more</div>
+    </div>
+  )
+}
+
+export default FactorySummary
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,14 @@
 import React from "react"
-import firebase from "../services/firebase"
-import SEO from "../components/seo"
+import SEO from "../components/Seo"
+import FactoryList from "../components/FactoryList"
 
 const IndexPage = () => {
-  var db = firebase.firestore()
-
-  console.log(db)
-  console.log(db.collection("factories"))
-  db.collection("factories")
-    .get()
-    .then(querySnapshot => {
-      querySnapshot.forEach(doc => {
-        console.log(`${doc.id} => ${doc.data().name}`)
-      })
-    })
   return (
     <div>
       <SEO title="Home" />
 
       <h1>This is sourceful</h1>
+      <FactoryList />
     </div>
   )
 }
