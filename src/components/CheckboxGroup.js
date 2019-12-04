08dfc459224966cd401d80ef947c6f292177dabb
Viktorija Valsoe <viktorijavalsoe@gmail.com>
2019-12-04 14:44:26 +0100

    removed material ui useStyles

--- src/components/CheckboxGroup.js
+++ src/components/CheckboxGroup.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
@@ -7,38 +6,23 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import PropTypes from 'prop-types';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  formControl: {
-    margin: theme.spacing(3),
-  },
-}));
-
-const CheckboxesGroup = ({ checkboxOptions, name, onChange }) => {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <FormControl component="fieldset" className={classes.formControl}>
-        <FormLabel component="legend">Certificates</FormLabel>
-        <FormGroup>
-          {checkboxOptions.map((option) => (
-            <FormControlLabel
-              key={option.value}
-              control={<Checkbox onChange={onChange} value={option.value} />}
-              label={option.label}
-              name={name}
-            />
-          ))}
-        </FormGroup>
-      </FormControl>
-    </div>
-  );
-};
+const CheckboxesGroup = ({ checkboxOptions, name, onChange }) => (
+  <div>
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Certificates</FormLabel>
+      <FormGroup>
+        {checkboxOptions.map((option) => (
+          <FormControlLabel
+            key={option.value}
+            control={<Checkbox onChange={onChange} value={option.value} />}
+            label={option.label}
+            name={name}
+          />
+        ))}
+      </FormGroup>
+    </FormControl>
+  </div>
+);
 
 export default CheckboxesGroup;
 
--- src/components/PrimaryButton.js
+++ src/components/PrimaryButton.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
-  },
-}));
-
-const PrimaryButton = ({ label, clearFilter }) => {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <Button variant="contained" color="primary" onClick={clearFilter}>
-        {label}
-      </Button>
-    </div>
-  );
-};
-
+const PrimaryButton = ({ label, clearFilter }) => (
+  <div>
+    <Button variant="contained" color="primary" onClick={clearFilter}>
+      {label}
+    </Button>
+  </div>
+);
 export default PrimaryButton;
 
 PrimaryButton.propTypes = {
