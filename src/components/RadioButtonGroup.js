ea5252b2f9555653b9e49740b35f8a8f9cc2c6ff
Viktorija Valsoe <viktorijavalsoe@gmail.com>
2019-12-04 14:44:26 +0100

    removed material ui useStyles

--- src/components/RadioButtonGroup.js
+++ src/components/RadioButtonGroup.js
@@ -1,41 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(3),
-  },
-}));
-
 const RadioButtonsGroup = ({
  options, formLabel, name, onChange 
-}) => {
-  const classes = useStyles();
-
-  return (
-    <div>
-      <FormControl component="fieldset" className={classes.formControl}>
-        <FormLabel component="legend">{formLabel}</FormLabel>
-        <RadioGroup name={name} onChange={onChange}>
-          {options.map((option) => (
-            <FormControlLabel
-              key={option.value}
-              value={option.value}
-              control={<Radio />}
-              label={option.label}
-            />
-          ))}
-        </RadioGroup>
-      </FormControl>
-    </div>
-  );
-};
+}) => (
+  <div>
+    <FormControl component="fieldset">
+      <FormLabel component="legend">{formLabel}</FormLabel>
+      <RadioGroup name={name} onChange={onChange}>
+        {options.map((option) => (
+          <FormControlLabel
+            key={option.value}
+            value={option.value}
+            control={<Radio />}
+            label={option.label}
+          />
+        ))}
+      </RadioGroup>
+    </FormControl>
+  </div>
+);
 export default RadioButtonsGroup;
 
 RadioButtonsGroup.propTypes = {
