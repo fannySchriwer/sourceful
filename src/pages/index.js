f47d380a2aaec607cadc40a9e0b8a792ac317440
Viktorija Valsoe <viktorijavalsoe@gmail.com>
2019-12-05 16:48:11 +0100

    moved sign in and sign up to compoennts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import SEO from '../components/seo';
+import SEO from '../components/Seo';
 import FilterFactoriesForm from '../components/FilterFactoriesForm';
+import SignUp from '../components/Signup';
+import Login from '../components/Login';
 
 const IndexPage = () => (
   <div>
     <SEO title="Home" />
 
     <h1>This is sourceful</h1>
+    <SignUp />
+    <Login />
     <FilterFactoriesForm />
   </div>
 );
