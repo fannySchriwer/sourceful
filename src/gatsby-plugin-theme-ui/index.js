606d0256e87e16d223273245bb3113a2c13bfce7
Fanny Schriwer <fanny_schriwer@hotmail.com>
2019-12-12 11:43:01 +0100

    refactur primary button

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,8 +1,10 @@
 export default {
   colors: {
+    lightGrey: '#F2F2F2',
     text: '#182628',
     primary: '#005467',
     secondary: '#B6D6CB',
+
   },
   fontWeights: {
     body: 400,
@@ -13,7 +15,20 @@ export default {
     body: 1.5,
     heading: 1.125,
   },
-  radii: ['5px', '30px'],
+  buttons: {
+    primary: {
+      bg: 'primary',
+      color: 'white',
+      paddingTop: 2,
+      paddingBottom: 2,
+      paddingRight: 4,
+      paddingLeft: 4,
+      border: 'none',
+      borderRadius: 4,
+      color: 'lightGrey'
+    }
+  },
+  radii: ['5px', '10px', '20px', '30px', '40px'],
   fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
   sizes: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
