ef0aa044e1b5c7166a2dcb867960fb26fcd3487e
Fanny Schriwer <fanny_schriwer@hotmail.com>
2019-12-16 21:56:51 +0100

    add factory counter component to render different paragraphs based on object length

--- src/components/FactoryList.js
+++ src/components/FactoryList.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FactorySummary from './FactorySummary';
+import FactoryListCounter from './FactoryListCounter';
 
-const FactoryList = ({ factories }) => (
-  <div>
-    <h1>Factories</h1>
+const FactoryList = ({ factories }) => {
+  const nrOfFactories = Object.keys(factories).length;
+  return(
+    <div>
+      <h1>Factories</h1>
+      <FactoryListCounter nrOfFactories={nrOfFactories} />
 
-    {factories.map((factory) => (
-      <FactorySummary key={factory.id} factory={factory} />
-    ))}
-  </div>
-);
+      {factories.map((factory) => (
+        <FactorySummary key={factory.id} factory={factory} />
+      ))}
+    </div>
+  )};
+
 export default FactoryList;
 FactoryList.propTypes = {
   factories: PropTypes.instanceOf(Array).isRequired,
