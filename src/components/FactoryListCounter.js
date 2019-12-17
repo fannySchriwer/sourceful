53abb30525dbeceb9667bb1c5cc590b8998041d8
Viktorija Valsoe <viktorijavalsoe@gmail.com>
2019-12-17 14:22:23 +0100

    style nr of factories"

--- src/components/FactoryListCounter.js
+++ src/components/FactoryListCounter.js
@@ -1,22 +1,24 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
 import PropTypes from 'prop-types';
 
 const FactoryListCounter = ({ nrOfFactories }) => {
-  return(
-    <div>
-      <p>{nrOfFactories > 1 &&
-        `${nrOfFactories} factories found`}
-      </p>
-      <p>{nrOfFactories === 1 &&
-        `${nrOfFactories} factory found`}
-      </p>
-      <p>{nrOfFactories === 0 &&
-        'No factories found, try clearing your filters'}
-      </p>
-    </div>
-  )};
+	return (
+		<div>
+			<p sx={{ fontStyle: 'italic', color: 'primary' }}>
+				{nrOfFactories > 1 && `${nrOfFactories} factories found`}
+			</p>
+			<p sx={{ fontStyle: 'italic', color: 'primary' }}>
+				{nrOfFactories === 1 && `${nrOfFactories} factory found`}
+			</p>
+			<p sx={{ fontStyle: 'italic', color: 'primary' }}>
+				{nrOfFactories === 0 && 'No factories found, try clearing your filters'}
+			</p>
+		</div>
+	);
+};
 
 export default FactoryListCounter;
 FactoryListCounter.propTypes = {
-  nrOfFactories: PropTypes.number.isRequired,
+	nrOfFactories: PropTypes.number.isRequired
 };
