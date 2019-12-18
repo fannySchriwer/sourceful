f9f17010df6dcfa4151a53e8533f09a29a40737d
Fanny Schriwer <fanny_schriwer@hotmail.com>
2019-12-18 17:20:58 +0100

    remove signup from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@
 import { jsx } from 'theme-ui';
 import { Fragment } from 'react';
 import FilterFactoriesForm from '../components/FilterFactoriesForm';
-import Signup from '../components/Signup';
 import Layout from '../components/Layout';
 import Hero from '../components/Hero';
 
@@ -11,7 +10,6 @@ const IndexPage = () => (
 		<Layout>
 			<Hero />
 			<main>
-				<Signup />
 				<FilterFactoriesForm />
 			</main>
 		</Layout>
