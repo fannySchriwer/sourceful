aeb0b3b5e0923094560c6eed15033c8710c104a5
Fanny Schriwer <fanny_schriwer@hotmail.com>
2019-12-23 12:30:41 +0100

    add input field component, add my list header dato in query. add styling to myfactory

---
+++ src/components/InputField.js
@@ -0,0 +1,35 @@
+/** @jsx jsx */
+import { jsx, Styled } from 'theme-ui';
+import PropTypes from 'prop-types';
+import TextField from '@material-ui/core/TextField';
+
+const InputField = ({ inputLabel, onChange }) => {
+  return (
+    <div
+      sx={{
+        display: 'flex',
+        flexDirection: 'column'
+      }}
+    >
+      <label id={name} htmlFor={inputLabel}>
+        <Styled.h4
+          sx={{
+            marginBottom: 2
+          }}
+        >
+          {inputLabel}
+        </Styled.h4>
+      </label>
+
+      <TextField id="outlined-basic" variant="outlined" onChange={onChange} />
+
+    </div>
+  )
+};
+
+export default InputField;
+InputField.propTypes = {
+	inputLabel: PropTypes.string.isRequired,
+	onChange: PropTypes.func.isRequired,
+};
+
