1aeb51ab0055b8be87aed5c01161204aea6c87e6
Viktorija Valsoe <viktorijavalsoe@gmail.com>
2020-01-04 17:29:48 +0000

    added getMyList hook to get real life update for myList

--- src/templates/list.js
+++ src/templates/list.js
@@ -1,14 +1,15 @@
 /** @jsx jsx */
 import { jsx, Styled } from 'theme-ui';
-import { graphql } from 'gatsby';
 import MyFactory from '../components/MyFactory';
 import Layout from '../components/Layout';
 import HeaderContainer from '../components/HeaderContainer';
 import BackgroundImg from '../components/BackgroundImg';
 import CardContainer from '../components/CardContainer';
 import InputField from '../components/InputField';
+import useMyGetList from '../hooks/useGetMylist';
 
-const List = ({ data: { user, datoCmsMyList } }) => {
+const List = () => {
+	const { myList } = useMyGetList();
 
 	function handleChange(event) {
 		event.preventDefault();
@@ -33,12 +34,11 @@ const List = ({ data: { user, datoCmsMyList } }) => {
 							'middle/outer-start/bottom/outer-end',
 							'middle-start/outer-start/bottom/outer-end'
 						],
-						zIndex: 2,
 						paddingX: [ 3, 5, 6 ],
 						marginTop: [ 3, 6, 6 ],
-						display: ['block', null, 'flex'],
+						display: [ 'block', null, 'flex' ],
 						flexDirection: 'column',
-						alignItems: 'flex-start',
+						alignItems: 'flex-start'
 					}}
 				>
 					<Styled.h1
@@ -47,59 +47,28 @@ const List = ({ data: { user, datoCmsMyList } }) => {
 							textAlign: 'center'
 						}}
 					>
-						{datoCmsMyList.title}
+						{'title'}
 					</Styled.h1>
-					<div sx={{ width: '250px', marginRight:  ['auto', null, 0], marginLeft: ['auto', null, 0] }}>
-						<InputField
-							inputLabel={datoCmsMyList.filterTitle}
-							onChange={handleChange}
-							placeholder='Search'
-						/>
+					<div sx={{ width: '250px', marginRight: [ 'auto', null, 0 ], marginLeft: [ 'auto', null, 0 ] }}>
+						<InputField inputLabel={'filterTitle'} onChange={handleChange} placeholder="Search" />
 					</div>
 				</div>
 			</HeaderContainer>
+
 			<section
 				sx={{
 					paddingX: [ 4, 5, 6 ],
 					marginTop: [ '-300px', '-170px' ]
 				}}
 			>
-				<CardContainer>
-					{user.childrenList.map((factory) => <MyFactory key={factory.id} factory={factory} />)}
-				</CardContainer>
+				{myList ? (
+					<CardContainer>
+						{myList.map((factory) => <MyFactory key={factory.id} factory={factory} />)}
+					</CardContainer>
+				) : null}
 			</section>
 		</Layout>
 	);
 };
 
 export default List;
-
-export const pageQuery = graphql`
-	query($id: String!) {
-		user(id: { eq: $id }) {
-			id
-			email
-			childrenList {
-				address {
-					country
-				}
-				comment
-				category {
-					jersey
-					knit
-					woven
-				}
-				name
-				id
-				employee
-				producttype
-				factoryID
-				quantity
-			}
-		}
-		datoCmsMyList {
-			title
-			filterTitle
-		}
-	}
-`;
